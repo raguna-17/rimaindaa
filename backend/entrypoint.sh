2e1f3c53c03d9f5017f00aea06abd2bf018e9b1c
らぐな <pasupoto17@gmail.com>
2026-02-06 22:58:16 +0900

    初回コミット

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 python manage.py migrate --noinput
-python backend/init_demo_user.py
+python init_demo_user.py       # ← backend直下ならパスはこれ
 python manage.py collectstatic --noinput
 gunicorn pro.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 3
