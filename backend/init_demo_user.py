cea36092b00a606e0103882c536ea1c32a3b73f3
らぐな <pasupoto17@gmail.com>
2026-02-07 10:07:43 +0900

    初回コミット

--- backend/init_demo_user.py
+++ backend/init_demo_user.py
@@ -1,4 +1,11 @@
 # backend/init_demo_user.py
+import os
+import django
+
+# Django を初期化
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")  # ←自分の settings に置き換える
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -8,6 +15,7 @@ User.objects.get_or_create(
     username="raguna",
     defaults={"email": "demo@example.com"}
 )
+
 user = User.objects.get(username="raguna")
 if not user.has_usable_password():
     user.set_password("kaibasensei")
